f81daeaca182721ccaf6b7ac558d47d30175ea29
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-03-13 07:38:32 +0100

    Start of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+
+    fetch(
+      'https://arbeidsplassen.nav.no/public-feed/api/v1/ads?size=10&page=1',
+      {
+        method: 'GET',
+        headers: {
+          Authorization:
+            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwdWJsaWMudG9rZW4udjFAbmF2Lm5vIiwiYXVkIjoiZmVlZC1hcGktdjEiLCJpc3MiOiJuYXYubm8iLCJpYXQiOjE1NTc0NzM0MjJ9.jNGlLUF9HxoHo5JrQNMkweLj_91bgk97ZebLdfx3_UQ',
+        },
+      }
+    ).then((res) =>
+    res.json().then((data) => {
+      console.log(data);      
+    })
+  );
+}, []);
+
+    
+  
   return (
     <div className="App">
       <header className="App-header">
