accdcc79e54216c064a364391e2a826430720c1a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-03-14 03:30:52 +0100

    Add typography with Nav font and background color

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Jobs from './components/Jobs/Jobs';
 
+import './App.css';
+
 const App: React.FC = () => (
   <div className="App">
     <Jobs />
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+// import Logo from '../img/Logo.png';
+
+const Navbar:React.FC = () => (
+  <div>
+    <nav>
+      <div className="logo">En tjeneste fra</div>
+      Logo
+    </nav>
+  </div>
+);
+
+export default Navbar;
