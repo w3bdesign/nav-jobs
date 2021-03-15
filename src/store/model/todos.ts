500ae17ead72dc2a7d5e4606efc8bb083d69effa
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-03-15 04:00:10 +0100

    Finish setting up state model

--- src/store/model/todos.ts
+++ src/store/model/todos.ts
@@ -1,50 +1,29 @@
 /* eslint-disable no-param-reassign */
 import { Action, action } from 'easy-peasy';
 
-/**
- * Interface for completeTodo with rowIndex: number
- */
-interface ICompleteTodo {
-  rowIndex: number;
+export interface modalModel {
+  /**
+   * List of modalItems.
+   */
+  modalItems: (string | boolean)[][];
+  /**
+   * Action to add modalitems that we will send to modal
+   */
+  addModalItems: Action<modalModel, string>;
+
+  /**
+   * Action to save and persist modalitems
+   */
+  saveModalItem: Action<modalModel, string>;
 }
 
-export interface TodosModel {
-  /**
-   * List of todoItems.
-   */
-  todoItems: (string | boolean)[][];
-  /**
-   * Action to add a todo to todoItems array
-   */
-  addTodo: Action<TodosModel, string>;
-  /**
-   * Action to complete a todo in todoItems array
-   */
-  completeTodo: Action<TodosModel, ICompleteTodo>;
-  /**
-   * Action to delete all todos from todoItems array
-   */
-  deleteAllTodos: Action<TodosModel>;
-  /**
-   * Action to delete a single todo from todoItems array
-   */
-  deleteTodo: Action<TodosModel, ICompleteTodo>;
-}
-
-const todosModel: TodosModel = {
-  todoItems: [['Test', new Date().toLocaleString('no-NO'), false]],
-  addTodo: action((state, payload) => {
-    state.todoItems.push([payload, new Date().toLocaleString('no-NO'), false]);
+const todosModel: modalModel = {
+  modalItems: [],
+  addModalItems: action((state, payload) => {
+    state.modalItems.push([payload, new Date().toLocaleString('no-NO'), false]);
   }),
-  completeTodo: action((state, { rowIndex }) => {
-    state.todoItems[rowIndex][2] = true;
-  }),
-  deleteTodo: action((state, { rowIndex }) => {
-    state.todoItems.splice(rowIndex, 1);
-  }),
-
-  deleteAllTodos: action((state) => {
-    state.todoItems.length = 0;
+  saveModalItem: action((state, payload) => {
+    state.modalItems.push([payload, new Date().toLocaleString('no-NO'), false]);
   }),
 };
 
