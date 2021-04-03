a0e7ab9836ece60d9f0b8c49c60e6af989a6cef9
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-04-03 06:30:05 +0200

    Fix Jest errors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { StoreProvider } from 'easy-peasy';
 
+import store from './store/index';
 import App from './App';
 
 test('renders Venter', () => {
-  render(<App />);
+  render(<StoreProvider store={store}><App /></StoreProvider>);
   const linkElement = screen.getByText(/Venter/i);
   expect(linkElement).toBeInTheDocument();
 });
