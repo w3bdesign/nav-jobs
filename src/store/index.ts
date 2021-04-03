1ab77fa3ea0e74d7d3f1109429d24499c7d8e170
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-04-03 21:42:31 +0200

    Remove unused code

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,6 @@
-// import { createStore, persist } from 'easy-peasy';
 import { createStore } from 'easy-peasy';
 
 import model from './model/index';
-
-/* const store = createStore(
-  persist({ model }),
-); */
 
 const store = createStore(model);
 
