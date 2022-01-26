45f1339d0d70069a44886b938391d6330ae23aea
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-26 04:39:56 +0100

    Legg til title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import type { NextPage } from "next";
 
 import JobListings from "../components/JobListings/JobListings";
@@ -6,6 +8,10 @@ import Navbar from "../components/Navbar/Navbar";
 const Home: NextPage = () => {
   return (
     <div id="root">
+      <Head>
+        <title>NAV Jobb Utforsker</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navbar />
       <JobListings />
     </div>
