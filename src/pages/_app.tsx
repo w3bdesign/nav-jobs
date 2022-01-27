dd880b42734aa1ba9454f786afc40da82e22167f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-28 00:07:19 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,8 @@ import '@navikt/ds-css-internal'
 import '../assets/scss/App.scss'
 import '../assets/css/animate.min.css'
 
-import { AppProps, NextPage } from 'next'
+import { NextPage } from 'next'
+import { AppProps } from 'next/app'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }): JSX.Element => {
   return (
