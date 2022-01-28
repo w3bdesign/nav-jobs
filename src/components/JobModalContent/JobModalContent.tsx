7ed99c5bcc07abebfd5928fe3ee3a11624069589
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-01-28 23:34:21 +0100

    Kebab-case

--- src/components/JobModalContent/JobModalContent.tsx
+++ src/components/JobModalContent/JobModalContent.tsx
@@ -13,9 +13,9 @@ const JobModalContent = ({ name, description, extent, applicationDue, closeModal
       <Button className={style['lukke-knapp']} variant="danger" onClick={closeModal}>
         Lukk
       </Button>
-      <div className={style.jobDiv}>
-        <span className={style.jobContent}>
-          <span className={style.jobTitle}>
+      <div className={style['job-div']}>
+        <span className={style['job-content']}>
+          <span className={style['job-title']}>
             {name}
             <br />
             {extent}
