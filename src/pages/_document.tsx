a95e69765f8c5d96b1dcfcbb7d507b12c2e517b4
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-18 20:20:41 +0100

    crossOrigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,8 +14,8 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
           <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="use-credentials" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />
           <link
             href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap"
             rel="stylesheet"
